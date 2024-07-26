65f19c47b83e2f7a8a040090f5d495c79be9b7b6
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-07-26 11:54:56 +0530

    feat: added cloudflare-workers in templates

---
+++ templates/cloudflare-workers/src/db/schema.ts
@@ -0,0 +1,9 @@
+import { boolean, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+
+export const todos = pgTable("todos", {
+  id: serial("id").primaryKey(),
+  status: boolean("status").notNull().default(false),
+  message: text("message").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
